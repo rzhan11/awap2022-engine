c3fa64d2de11a630df4c1576acbd647b3dca716a
Richard Zhan <rzhan11@Richards-MBP.lan>
2021-12-22 09:20:30 -1000

    simple bot with towers

--- bots/bot.py
+++ bots/bot.py
@@ -15,14 +15,68 @@ class MyPlayer(Player):
 
         return
 
+
+
+    def find_path(self, start, dest):
+        prev = {start: None}
+        q = [start]
+        index = 0
+        while index < len(q):
+            state = q[index]
+            if state == dest:
+                path = [state]
+                cur = state
+                while cur != start:
+                    cur = prev[cur]
+                    path += [cur]
+
+                path = path[::-1]
+                return path
+
+            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                nx, ny = state[0] + dx, state[1] + dy
+                if not (0 <= nx < self.width and 0 <= ny < self.height):
+                    continue
+                if (nx, ny) in prev:
+                    continue
+                prev[(nx, ny)] = state
+                q += [(nx, ny)]
+
+
+            index += 1
+
+        return None
+
+
     def play_turn(self, turn_num, map, my_info):
         print("turn", turn_num, my_info)
 
-        width = len(map)
-        height = len(map[0])
+        us = my_info.team
+        self.width = len(map)
+        self.height = len(map[0])
 
-        x, y = random.randrange(0, width), random.randrange(0, height)
-        self.build(StructureType.ROAD, x, y)
+        gens = []
+        pops = []
+        for x in range(self.width):
+            for y in range(self.height):
+                if map[x][y].population > 0 and map[x][y].structure is None:
+                    pops += [(x, y)]
+
+                st = map[x][y].structure
+                if st is not None:
+                    if st.team == us and st.type == StructureType.GENERATOR:
+                        gens += [(x, y)]
+
+        if len(pops) > 0:
+            dest = pops[0]
+            path = self.find_path(gens[0], dest)
+            for loc in path:
+                if loc == dest:
+                    t = StructureType.TOWER
+                else:
+                    t = StructureType.ROAD
+                self.build(t, loc[0], loc[1])
+                # print(self._to_build[0])
 
 
         return
