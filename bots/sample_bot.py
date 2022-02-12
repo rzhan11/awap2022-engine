8e027283450e7655414e0ab2e8f93932570de8e6
Richard Zhan <rzhan@andrew.cmu.edu>
2022-02-12 11:03:44 -0500

    sample bot

---
+++ bots/sample_bot.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.insert(1, "../src")
+
+import random
+
+from player import *
+from structure import *
+from game_constants import GameConstants as GC
+
+class MyPlayer(Player):
+
+    def __init__(self):
+        print("Init")
+        self.turn = 0
+
+        return
+
+
+    def play_turn(self, turn_num, map, my_info):
+
+
+        return
